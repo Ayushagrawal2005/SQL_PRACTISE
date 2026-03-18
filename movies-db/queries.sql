6c73084f8063e81a83f8d1723fdada393f897e91
ayush <aayush.2005sng@gmail.com>
2026-03-18 12:11:21 +0530

    Initial commit

--- movies-db/queries.sql
+++ movies-db/queries.sql
@@ -20,3 +20,34 @@ FROM movies;
 -- Retrieve all columns from the movies table
 SELECT *
 FROM movies;
+SELECT *
+FROM movies
+WHERE id = 6;
+
+SELECT *
+FROM movies
+WHERE year BETWEEN 2000 AND 2010;
+
+SELECT *
+FROM movies
+WHERE year NOT BETWEEN 2000 AND 2010;
+
+SELECT title, year
+FROM movies
+WHERE id <= 5;
+
+SELECT *
+FROM movies
+WHERE title LIKE 'Toy Story%';
+
+SELECT *
+FROM movies
+WHERE director = 'John Lasseter';
+
+SELECT title, director
+FROM movies
+WHERE director != 'John Lasseter';
+
+SELECT *
+FROM movies
+WHERE title LIKE 'WALL-%';
