96373c2d5cfe80b570c9339b089db1b3a670c781
Ayushagrawal2005 <aayush.2005sng@gmail.com>
2026-03-18 12:42:25 +0530

    Delete movies-db/schema.sql

--- movies-db/schema.sql
+++
@@ -1,9 +0,0 @@
--- Schema for the movies database
--- Creates the movies table with title, director, and year columns
-
-CREATE TABLE IF NOT EXISTS movies (
-    id       INTEGER PRIMARY KEY AUTOINCREMENT,
-    title    TEXT    NOT NULL,
-    director TEXT    NOT NULL,
-    year     INTEGER NOT NULL
-);
